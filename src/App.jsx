814166ccd3801c5a547edb7b79aeda596fe17491
lauru balauru <laurentiupopovici2608@gmail.com>
2025-11-17 23:08:36 +0200

    added so many things I can't even list them all

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [currentPage, setCurrentPage] = useState("home"); 
 
   if (currentPage === "lesson") {
-    return <Lesson goHome={() => setCurrentPage("home")} />;
+	return <Lesson goHome={() => setCurrentPage("home")} />;
   }
   if (currentPage === "curriculum") {
     return <Curriculum goHome={() => setCurrentPage("home")} />;
@@ -20,24 +20,24 @@ function App() {
 
   return (
     <div className="min-h-screen bg-black-100 flex flex-col items-center justify-center p-6">
-      {/* Header */}
+      
       <header className="w-full max-w-4xl text-center mb-12">
-        <h1 className="text-5xl font-bold text-green-600 mb-4">
-          CyberStars
-        </h1>
-        <p className="text-gray-700 text-lg">
-          Learn to code for free
-        </p>
+			<h1 className="text-5xl font-bold text-green-600 mb-4">
+				CyberStars
+			</h1>
+			<p className="text-gray-700 text-lg">
+				Learn to code for free
+			</p>
       </header>
 
-      {/* Buttons */}
+     
       <div className="flex flex-col sm:flex-row gap-4 mb-12">
-        <button className="px-6 py-3 bg-green-600 text-white rounded hover:bg-green-700 transition" onClick={() => setCurrentPage("getstarted")}>
-          Get Started
-        </button>
-        <button className="px-6 py-3 border border-green-600 text-green-600 rounded hover:bg-green-50 transition"  onClick={() => setCurrentPage("curriculum")}>
-          Explore Curriculum
-        </button>
+			<button className="px-6 py-3 bg-green-600 text-white rounded hover:bg-green-700 transition" onClick={() => setCurrentPage("getstarted")}>
+				Get Started
+			</button>
+			<button className="px-6 py-3 border border-green-600 text-green-600 rounded hover:bg-green-50 transition"  onClick={() => setCurrentPage("curriculum")}>
+				Explore Curriculum
+			</button>
       </div>
 
      
--- src/Curriculum.jsx
+++ src/Curriculum.jsx
@@ -1,38 +1,90 @@
-function Curriculum({ goHome }) {
-  const topics = [
-    "Variables",
-    "Print",
-    "Types",
-    "if",
-    "else if",
-    "else",
-    "for",
-    "while",
-    "switch",
-  ];
+import { useState } from "react";
 
+function Curriculum({ goHome }) {
+
+	const [selected, setSelected] = useState(null);
+
+	const curriculum = [
+		{
+			title: "C",
+			description: "The Foundation of Programming",
+			lessons: [
+				"variables", "print", "types", "if", "else if", "else", "for", "while", "switch"
+			]
+		},
+		{
+			title: "Java",
+			description: "Object Oriented Programming",
+			lessons: [
+				"variables", "print", "if", "else if", "else", "for", "while", "Class", "Object"
+			]
+		},
+		{
+			title: "Linux",
+			description: "Terminal & Bash Scripting",
+			lessons: [
+				"TBC1", "TBC2", "TBC3", "TBC4"
+			]
+		}
+	]
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
-      <h1 className="text-3xl font-bold text-green-600 mb-6">What you can learn:</h1>
+      <h1 className="text-3xl font-bold text-green-600 mb-6">Hmm... let's see</h1>
 
   
 
-      <section className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-3 gap-6 text-center">
-              <div className="p-6 bg-white rounded shadow hover:shadow-lg transition">
-                <h3 className="text-xl font-bold mb-2">Learn C</h3>
-                <p className="text-gray-600">The Foundation of Programming</p>
-              </div>
-              <div className="p-6 bg-white rounded shadow hover:shadow-lg transition">
-                <h3 className="text-xl font-bold mb-2">Learn Java</h3>
-                <p className="text-gray-600">Object Oriented Programming</p>
-              </div>
-              <div className="p-6 bg-white rounded shadow hover:shadow-lg transition">
-                <h3 className="text-xl font-bold mb-2">Learn Linux</h3>
-                <p className="text-gray-600">Terminal & Bash Scripting</p>
-              </div>
-            
-            </section>
-      {/* Buton Home */}
+		<section className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-3 gap-6 text-center">
+		
+			{curriculum.map((entry, i) => {
+				return (
+					<div className="p-6 bg-white rounded shadow hover:shadow-lg transition">
+						<div key={i}>
+							<h3 className="text-xl font-bold mb-2">{entry.title}</h3>
+							<p className="text-gray-600">{entry.description}</p>
+							<button className="mt-4 px-4 py-2 bg-green-600 text-white rounded-lg shadow hover:bg-green-700 hover:shadow-md transition"
+							onClick={() => setSelected(entry)}>
+								What will I learn?
+							</button>
+
+						</div>
+					</div> 
+
+				)
+			})}
+				     
+		</section>
+		{selected && (
+			<div
+				className="fixed inset-0 bg-black/20 backdrop-blur-sm flex items-center justify-center"
+				onClick={() => setSelected(null)}
+			>
+				<div
+				className="bg-white p-6 rounded-xl shadow-xl max-w-md w-full"
+				onClick={(e) => e.stopPropagation()}
+				>
+				<h2 className="text-2xl font-bold mb-4 text-green-600">
+					{selected.title}
+				</h2>
+				<p className="text-gray-700 mb-4">{selected.description}</p>
+
+				<ul className="list-disc pl-6 text-gray-800 space-y-1">
+					{selected.lessons.map((lesson, i) => (
+					<li key={i}>{lesson}</li>
+					))}
+				</ul>
+
+				<button
+					className="mt-6 px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
+					onClick={() => setSelected(null)}
+				>
+					Close
+				</button>
+				</div>
+			</div>
+		)}
+
+
+    
       <button
         onClick={goHome}
         className="mt-8 px-6 py-3 bg-green-600 text-white rounded hover:bg-green-700 transition"
